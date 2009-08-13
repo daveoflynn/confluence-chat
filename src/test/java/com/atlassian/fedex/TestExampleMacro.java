d7ede2ef95cbc79f25b0696899ef91111fc4317b
= <doflynn@badger-badger.local>
2009-08-13 17:29:25 +1000

    You think we'd have learned to escape strings by now.

---
+++ src/test/java/com/atlassian/fedex/TestExampleMacro.java
@@ -0,0 +1,14 @@
+package com.atlassian.fedex;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link com.atlassian.fedex.ExampleMacro}
+ */
+public class TestExampleMacro extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
