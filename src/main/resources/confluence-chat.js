d60f4f6403b31421876565cdd77cb4199a8024bd
Lachlan Hardy <lachlan@lachstock.com.au>
2009-08-14 11:16:06 +1000

    rough cut of adding hover state - still buggy

--- src/main/resources/confluence-chat.js
+++ src/main/resources/confluence-chat.js
@@ -1,5 +1,11 @@
 AJS.toInit(function($) {
 
+  // To get the chat URL: /rest/chat/1/lhardy/chat
+  // where lhardy is the name of the user you want to chat with.
+  
+  // presence will be available at /rest/chat/1/lhardy when Dave gets it working.
+
+  // Wax on, wax off
   AJS.$(".confluence-userlink").hover(
     function () {
       setTimeout(checkStatus, 1000); 
@@ -9,11 +15,33 @@ AJS.toInit(function($) {
     }
   );
   
-  var checkStatus = function(testInterval){
-    var profile = AJS.$(".profile-macro", ".ajs-content-hover");
-    if (profile) {
-      console.log("do something useful here");
+  // Checks and displays chat status
+  var checkStatus = function(){
+    
+    // TODO: only target THIS user's .ajs-content-hover
+    var $profile = AJS.$(".ajs-content-hover");
+    if ($profile) {
+      $profile.toggleClass("confluence-chat-plugin"); // Provide scope for CSS
+      
+      var linkText = "Chat with " + AJS.$("a.fn", $profile).text(),
+          linkHref =  "rest/chat/1/ahempel/chat"; // query this
+    
+      var $chatLink = AJS.$("<a class=\"plugin-chat-link\"></a>").attr("href", linkHref).append(linkText);
+      
+      AJS.$(".values", $profile).append($chatLink);
+      
+     // Call clearStatus TODO: make not buggy
+      $profile.add(AJS.$(".user-popup-menu-admin")).mouseleave(function() {
+        // console.log("called?");
+        setTimeout(clearStatus, 1000);
+      });
     }
+  };
+
+  // Clears all changes
+  var clearStatus = function() {
+      AJS.$(".plugin-chat-link").remove();
+      AJS.$(".profile-macro", ".ajs-content-hover").toggleClass("confluence-chat-plugin");
   };
 
 })(AJS.$);
