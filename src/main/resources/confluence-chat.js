1b4bdcbfc90888aaea128e0fbd0719cb7ab79d72
Lachlan Hardy <lachlan@lachstock.com.au>
2009-08-14 13:58:25 +1000

    It's alive

--- src/main/resources/confluence-chat.js
+++ src/main/resources/confluence-chat.js
@@ -1,8 +1,5 @@
 AJS.toInit(function($) {
 
-  // To get the chat URL: /rest/chat/1/lhardy/chat
-  // where lhardy is the name of the user you want to chat with.
-  
   // presence will be available at /rest/chat/1/lhardy when Dave gets it working.
 
   // Wax on, wax off
@@ -14,7 +11,7 @@ AJS.toInit(function($) {
       
       setTimeout(function () {
         checkStatus(username, userIndex);
-      }, 1000); 
+      }, 500); 
     }, 
     function () {
       // necessary?
@@ -26,24 +23,12 @@ AJS.toInit(function($) {
     var $profile = AJS.$("#content-hover-" + userIndex); // Target user-specific content-hover
     if ($profile) {
       $profile.toggleClass("confluence-chat-plugin"); // Provide scope for CSS
-      
-      
-      
-      var linkText = "Chat with " + AJS.$("a.fn", $profile).text(),
-          getChatURL = location.protocol + "//" + location.host + "/confluence/rest/chat/1/" + username + "/chat.json";
-          console.log(getChatURL);
+      var getChatURL = location.protocol + "//" + location.host + "/confluence/rest/chat/1/" + username + "/chat.json";
           
-          var linkHref = "test";
-          $.getJSON(getChatURL,
-            function(data){
-              alert(data);
-          });
-          console.log(linkHref);
-          
-    
-      var $chatLink = AJS.$("<a class=\"plugin-chat-link\"></a>").attr("href", linkHref).append(linkText);
-      
-      AJS.$(".values", $profile).append($chatLink);
+      $.getJSON(getChatURL,
+        function(data){
+          buildLink(data.url, $profile);
+      });
       
      // Call clearStatus TODO: make not buggy
       $profile.add(AJS.$(".user-popup-menu-admin")).mouseleave(function() {
@@ -52,6 +37,15 @@ AJS.toInit(function($) {
       });
     }
   };
+
+  var buildLink = function(url, $profile){
+    var linkText = "Chat with " + AJS.$("a.fn", $profile).text(),
+        $chatLink = AJS.$("<a class=\"plugin-chat-link\"></a>")
+                      .attr("href", url)
+                      .append(linkText);
+    
+    AJS.$(".values", $profile).append($chatLink);
+  }
 
   // Clears all changes
   var clearStatus = function() {
